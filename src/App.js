368c2a78a5ef01ff75bd59cca286ec04350ba003
radrigo97 <stitchofficial01@gmail.com>
2021-01-11 13:29:29 +0300

    redux-kanban install 2/4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ul>
+       {
+         props.task.map(el => <li key={el.name}>{el.name}</li>)
+       }
+     </ul>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  task: state.tasks
+})
+
+export default connect(mapStateToProps)(App);
