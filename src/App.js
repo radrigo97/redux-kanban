7e28df36c46a81d44bd1943120d842a5c51f196a
radrigo97 <stitchofficial01@gmail.com>
2021-01-12 18:00:47 +0300

    redux-kanban install 2/4

--- src/App.js
+++ src/App.js
@@ -10,14 +10,29 @@ function App(props) {
         setTask('')
     }
 
+    const up = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-short"
+         viewBox="0 0 16 16">
+        <path fillRule="evenodd"
+              d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+    </svg>)
+
+    const down = (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down"
+             viewBox="0 0 16 16">
+            <path fillRule="evenodd"
+                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+        </svg>)
+
     return (
         <div className="App">
             <ul>
                 <input type="text" value={task} onChange={e => setTask(e.target.value)}/>
                 <button onClick={addTask}>Add</button>
                 {
-                    props.task.map(el => <li key={el.name}>{el.name}
-                        <button onClick={() => props.upTask(el.id)}>Up</button>
+                    props.task.map((el, index) => <li key={el.id}>{el.name}
+                        <button onClick={() => props.upTask(index)}>{up}</button>
+                        <button onClick={() => props.downTask(index)}>{down}</button>
                     </li>)
                 }
             </ul>
@@ -31,7 +46,8 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
     addTask: (task) => dispatch({type: 'ADD_TASK', payload: task}),
-    upTask: (up) => dispatch({type: 'UP_TASK', payload: up})
+    upTask: (up) => dispatch({type: 'UP_TASK', payload: up}),
+    downTask: (down) => dispatch({type: 'DOWN_TASK', payload: down})
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import {composeWithDevTools} from "redux-devtools-extension";
 
 
 const initialState = {
-    tasks: [{name: 'hello'}]
+    tasks: [{name: 'hello', id: Math.random()}]
 }
 
 const reducers = (state = initialState, action) => {
@@ -13,6 +13,27 @@ const reducers = (state = initialState, action) => {
               ...state,
               tasks: [...state.tasks,{name: action.payload, id: Math.random()}]
           }
+
+      case 'UP_TASK':
+       return {
+           ...state,
+           tasks: state.tasks.map((el,i) => {
+               if (action.payload === i) return state.tasks[action.payload - 1]
+               if(action.payload - 1 === i) return state.tasks[action.payload]
+               return el
+           })
+       }
+
+      case 'DOWN_TASK':
+          return {
+              ...state,
+              tasks: state.tasks.map((el,i) => {
+                  if(action.payload === i) return state.tasks[action.payload + 1]
+                  if(action.payload + 1 === i) return state.tasks[action.payload]
+                      return el
+              })
+          }
+
       default:
           return state
   }
