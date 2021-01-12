b7729f3b7227fb7a5d53a642570a6554be0d3d31
radrigo97 <stitchofficial01@gmail.com>
2021-01-12 12:51:32 +0300

    redux-kanban install 2/4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import './App.css';
+import React, {useState} from 'react'
 import {connect} from "react-redux";
 
 function App(props) {
-  return (
-    <div className="App">
-     <ul>
-       {
-         props.task.map(el => <li key={el.name}>{el.name}</li>)
-       }
-     </ul>
-    </div>
-  );
+    const [task, setTask] = useState('')
+
+    const addTask = () => {
+        props.addTask(task)
+        setTask('')
+    }
+
+    return (
+        <div className="App">
+            <ul>
+                <input type="text" value={task} onChange={e => setTask(e.target.value)}/>
+                <button onClick={addTask}>Add</button>
+                {
+                    props.task.map(el => <li key={el.name}>{el.name}
+                        <button onClick={() => props.upTask(el.id)}>Up</button>
+                    </li>)
+                }
+            </ul>
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
-  task: state.tasks
+    task: state.tasks
 })
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+    addTask: (task) => dispatch({type: 'ADD_TASK', payload: task}),
+    upTask: (up) => dispatch({type: 'UP_TASK', payload: up})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
